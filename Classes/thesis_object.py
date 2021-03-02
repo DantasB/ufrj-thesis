7b363b02b19478b0c99146eb44924189b0de9010
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-03-01 21:26:45 -0300

    Adding Thesis Object

---
+++ Classes/thesis_object.py
@@ -0,0 +1,48 @@
+class Thesis():
+    """ Used to define all the characteristics of a thesis.
+
+    Args:
+        title (str): thesis title
+        authors (list): list of the authors who writed the thesis
+        advisors (list): list of advisors who helped this theses
+        url (str): url to the thesis pdf
+        keywords (list): keywords that relates to the thesis theme.
+        university (str): university name
+        institution (str): institution name
+        course (str): authors course name 
+        language (str): language that the thesis got published.
+        year (int): year that the thesis got published
+    """
+    def __init__(self, title, authors, advisors, url, keywords, university, institution, course, language, year):
+        self.title       = title 
+        self.url         = url
+        self.authors     = authors
+        self.advisors    = advisors
+        self.keywords    = keywords
+        self.university  = university
+        self.institution = institution
+        self.course      = course
+        self.language    = language
+        self.year        = year
+    
+    def __eq__(self, thesis):
+        if self.title == thesis.title:
+            return True
+        return False
+
+    def serialize(self):
+        """Serializes the Thesis Object
+
+        Returns:
+            dictionary: serialized object
+        """
+        return {"Title": self.title,
+                "Url": self.url,
+                "Authors": self.authors,
+                "Advisors": self.advisors,
+                "Keywords": self.keywords,
+                "University": self.university,
+                "Institution": self.institution,
+                "Course": self.course,
+                "Language": self.language,
+                "Year": self.year}

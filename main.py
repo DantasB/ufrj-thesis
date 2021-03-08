e25d980bb5cd855cb3ba1cbe34526d077569c8e3
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-03-08 14:21:24 -0300

    [main.py] Minor Changes.

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from SharedLibrary.mongo_utils import insert_document_on_mongo
 from SharedLibrary.env_utils import load_parameters       
 
 if __name__ == "__main__":
-    thesis_list = get_thesis_objects()
     no_error, collection = access_collection(load_parameters())
-
+    if not no_error:
+        return None
+    
+    thesis_list = get_thesis_objects()
     if no_error:
         for thesis in thesis_list:
             insert_document_on_mongo(collection, thesis.serialize())
